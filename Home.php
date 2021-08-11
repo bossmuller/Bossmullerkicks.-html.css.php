439931fdad1f96ef54ca252226624467e690b6d7
bossmuller <85559060+bossmuller@users.noreply.github.com>
2021-08-11 11:02:17 +0300

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,479 @@
+<?
+ session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bossmulerkicks</title>
+	<link rel="stylesheet" href="Style.css">
+	<link href="https://fonts.googleleapis.com/css2?family=Poppins:wght@300;400;500;600;700&dispaly=swap"rel="stylesheet">
+	<link rel="stylesheet" href ="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div class="header">
+<div class="container">
+	<div class="navbar">
+	    <div class="logo">
+		<img src="images/log.png" width="125px">
+        </div>
+     <nav>
+		<ul id="MenuItems">
+			<li><a href="Home.html">Home</a></li>
+			<li><a href="Products.html">Products</a></li>
+			<li><a href="About.html">About</a></li>
+			<li><a href="Contact Us.html">Contact Us</a></li>
+			<li><a href="Register.html">Register</a></li>
+			<li><a href="Login.html">Login</a></li>
+			<li><a href="Addcart.html">Add Cart <img src="images/bag-cart.png" width="30px" height="30px"> </a></li>
+			<li><a href="LogOut.php">LogOut</a></li>
+
+		</ul>
+	<img src="images/menu.png" class="menu" onclick="menutoggle()"> 	
+	</nav>
+	</div>
+  <div class="row">
+	<div class="col-2">
+		<h1>Welcome to Bossmullerkicks<br> Men's shoes store!</h1>
+		<p> Get your smart shoes of your choice at affordable price delivered at your place.</p>
+		<a href=""class="btn">Explore Now &#8594;</a>
+	</div>
+	<div class="col-2">
+		<img src="images/luku.jpg">
+	</div>
+  </div>
+</div>
+</div>
+	<!----- features categories----->
+	<div class="categories">
+		<div class="small-container">
+			<div class="row">
+			<div class="col-3">
+				<img src="images/dress shoes.jpg">
+			</div>
+			<div class="col-3">
+				<img src="images/green-suede.jpg">
+			</div>
+			<div class="col-3">
+				<img src="images/DeChina vendor.jpg">
+			</div>
+			<div class="col-3">
+				<img src="images/sky-garden.jpg">
+			</div>
+			<div class="col-3">
+				<img src="images/Quality.jpg">
+			</div>
+			<div class="col-3">
+				<img src="images/hot.jpg">
+			</div>
+		</div>
+		</div>
+		
+	</div>
+	<!----- features categories----->
+	<div class="small-container">
+		<h2 class="title">Featured Men's Shoes</h2>
+		<div class="row">
+			<div class="col-4">
+			<img src="images/black casual.jpg">
+			<h4>Black Casual Men Shoes </h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 4500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/coffee brown.jpg">
+			<h4>Coffee Brown </h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 3500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/Khaki Green.jpg">
+			<h4>Khaki Green Boots </h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-half-o"></i>
+			</div>
+
+			<P>KSH 4500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/leather-bullock formal shoes.jpg">
+			<h4>leather-bullock formal shoes</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-half-o"></i>
+			</div>
+
+			<P>KSH 5500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/sky-garden.jpg">
+			<h4>Sky Garden Official Lace Up</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 4319.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/safety shoes.jpg">
+			<h4>Running Shoes For Men(Black)</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 2500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/red sport.jpg">
+			<h4>Running Shoes For Men(Red)</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-half-o"></i>
+			</div>
+
+			<P>KSH 2500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/sport Lace.jpg">
+			<h4>Men's Sport Shoes lace Up</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 2500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+	</div>
+	<h2 class="title">Latest Men's Shoes</h2>
+<div class="row">
+			<div class="col-4">
+			<img src="images/Latest-1.jpg">
+			<h4>Solid Patent Leather Bow </h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 4650.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/Latest-2.jpg">
+			<h4>Black Taxedo </h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 6000.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/latest-3.jpg">
+			<h4>Men Sneakers Running Shoes </h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-half-o"></i>
+			</div>
+
+			<P>KSH 4500.00</P>
+			<p><button>Add to Cart</button></p>
+			
+		</div>
+		<div class="col-4">
+			<img src="images/latest-4.jpg">
+			<h4>Supreme</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-half-o"></i>
+			</div>
+
+			<P>KSH 3500.00</P>
+			<p><button>Add to Cart</button></p>
+		</div>
+		<div class="col-4">
+			<img src="images/latest-5.jpg">
+			<h4>Gucci- stylish Men's shoes</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 5000.00</P>
+			<p><button>Add to Cart</button></p>
+		</div>
+		<div class="col-4">
+			<img src="images/TAOTAO.jpg">
+			<h4>Men Sports Casual Shoes</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 699.00</P>
+			<p><button>Add to Cart</button></p>
+		</div>
+		<div class="col-4">
+			<img src="images/Delin.jpg">
+			<h4>Delin- China OEM Formal shoes</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-half-o"></i>
+			</div>
+
+			<P>KSH 3500.00</P>
+			<p><button>Add to Cart</button></p>
+		</div>
+		<div class="col-4">
+			<img src="images/latest-6.png">
+			<h4>Official shoes (Black)</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 2900.00</P>
+			<p><button>Add to Cart</button></p>
+		</div>
+		<div class="col-4">
+			<img src="images/latest-7.jpg">
+			<h4>Official shoes (Brown)</h4>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+
+			<P>KSH 2800.00</P>
+			<p><button>Add to Cart</button></p>
+		</div>
+		<!----offer---->
+	<div class="offer">
+		<div class="small-container">
+			<div class="row">
+				<div class="col-2">
+					<img src="images/exclusive.jpg" class="offer-img">
+				</div>
+               <div class="col-2">
+				<p>Exclusive Available on <b>Bossmullerkicks</b> </p>
+				<h1>Loafer Canvas</h1>
+				<small>The best price of <b>Special Offer(30% Off)</b>  casual mens loafers canvas shoes flat  sneakers DARK GREY by <b>Bossmulerkicks</b>in Kenya at <b>1399 Ksh.</b> </small><br>
+				<a href=""class="btn">Buy Now &#8594;</a>
+
+			    </div>
+	        </div>
+			
+		</div>		
+
+     </div>
+
+		
+			
+<!------- testimonial----->
+<div class="testimonial">
+	<div class="row">
+		<div class="col-3">
+			<i class="fa fa-quote-left"></i>
+			<p>Bossmullerkicks store has made all men to look elegant in their smart shoes and we are proud of Bossmullerkicks store.
+				</p>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+			<img src="images/Dela.jpg">
+			<h3>
+				Dela Queen
+			</h3>
+		</div>
+		<div class="col-3">
+			<i class="fa fa-quote-left"></i>
+			<p>Bossmullerkicks store has made all men to look elegant in their smart shoes and we are proud of Bossmullerkicks store.
+				</p>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+			<img src="images/boss.jpg">
+			<h3>
+				Joash Luten
+			</h3>
+		</div>
+		<div class="col-3">
+			<i class="fa fa-quote-left"></i>
+			<p>Bossmullerkicks store has made all men to look elegant in their smart shoes and we are proud of Bossmullerkicks store.
+				</p>
+			<div class="rating">
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star" ></i>
+				<i class="fa fa-star-o"></i>
+			</div>
+			<img src="images/king.jpg">
+			<h3>
+				Joel Smith
+			</h3>
+		</div>
+		
+		
+	</div>
+</div>
+<!--------brands----->
+<div class="brands">
+	<div class="small-container">
+		<div class="row">
+			<div class="col-5">
+				<img src="images/logo-6.png">
+			</div>
+			
+			<div class="col-5">
+				<img src="images/logo-1.png">
+			</div>
+			<div class="col-5">
+				<img src="images/logo-5.png">
+			</div>
+			<div class="col-5">
+				<img src="images/logo-2.jpg">
+			</div>
+		</div>
+
+	</div>
+</div>
+<!------footer----->
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="footer-col-1">
+				<h3>Visit Our Online Shop</h3>
+				<P> Get all of your choice in <b>Bossmullerkicks</b> at affordable price and very durable</P>
+			</div>
+			<div class="footer-col-2">
+				<h3>Bossmullerkicks Store</h3>
+				<P>Our Purpose Is To Sustainably Make the Pleasure and Benefits of all Men Accessible to the Many and all Men to Look Elegantly </P>
+			</div>
+			<div class="footer-col-3">
+				<h3>Useful Links</h3>
+				<ul>
+					<li>Coupons</li>
+					<li>Blog Post</li>
+					<li>Return Policy</li>
+					<li>Join Affiliate</li>
+				</ul>
+			</div>
+			<div class="footer-col-4">
+				<h3>Follow US</h3>
+				<ul>
+					<li>Facebook</li>
+					<li>Twitter</li>
+					<li>Instagram</li>
+					<li>YouTube</li>
+				</ul>
+			</div>
+		</div>
+		<hr>
+		<p class="copyright">Copyright 2021-Bossmullerkicks Store</p>
+	</div>
+</div>
+<!-------js for toggle menu--->
+<script>
+	var MenuItems= document.getElementById("MenuItems");
+	MenuItems.style.maxHeight="0px";
+
+	function menutoggle(){
+		if (MenuItems.style.maxHeight=="0px")
+			{
+				MenuItems.style.maxHeight=="200px";
+			else{
+				MenuItems.style.maxHeight=="0px";
+			}
+	}
+
+</script>
+</body>
+</html>
